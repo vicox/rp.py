73a30c3ddffdf3fbf5152490ae23ccbb9e412474
Georg Schmidl <georg.schmidl@vicox.net>
2024-05-18 16:40:31 +0200

    Add --list-tracks option

--- rp.py
+++ rp.py
@@ -7,12 +7,31 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('source')
 group = parser.add_mutually_exclusive_group()
-group.add_argument('-ld', '--list-dates', action='store_true', help='list all dates with number of tracks')
+group.add_argument(
+    '-lt',
+    '--list-tracks',
+    action='store_true',
+    help='list all tracks'
+)
+group.add_argument(
+    '-ld',
+    '--list-dates',
+    action='store_true',
+    help='list all dates with number of tracks'
+)
 args = parser.parse_args()
 
 source = args.source
 
-if args.list_dates:
+if args.list_tracks:
+    total_tracks = 0
+
+    for filename in os.listdir(source):
+        total_tracks +=1
+        print(filename)
+
+    print(f'Total tracks: {total_tracks}')
+elif args.list_dates:
     dates = set()
     tracks_per_date = dict()
 
@@ -25,6 +44,6 @@ if args.list_dates:
             tracks_per_date[date] = 1
         else:
             tracks_per_date[date] += 1
-    
+
     for date in sorted(dates):
         print(f'{date}: {tracks_per_date[date]} tracks')
